9cb9e91860c50f3e091da22c5953ac142f3ed7d2
mahikotahi <portilizenseason@gmail.com>
2024-05-11 15:31:59 -0600

    cant get it to work

--- Misc/Launcher/scenes/UpdateListOpener.gd
+++ Misc/Launcher/scenes/UpdateListOpener.gd
@@ -2,17 +2,18 @@ extends TextureButton
 
 @onready var update_thing = $"../UpdateThing"
 @onready var link_button = $"../UpdateThing/LinkButton"
+@onready var description = $"../UpdateThing/Description"
 
 func _ready():
 	link_button.anchors_preset = PRESET_CENTER
-	link_button.uri = load("assets/updates/current_update.txt")
+	# link_button.uri = load("assets/updates/current_update.txt")
+	
+	# description.text = load("assets/updates/current_update.txt")
+	
+	# print(load("updates/current_update.txt"))
+	description.text = "\ntest"
 
 func _on_pressed():
 	# load("assets/updates/current_update.txt")
 	update_thing.visible = true
 	# link_button.uri = load("assets/updates/current_update.txt")
-
-func load(fileN):
-	var file = FileAccess.open("red://"+fileN, FileAccess.READ)
-	var content = file.get_as_text()
-	return content
