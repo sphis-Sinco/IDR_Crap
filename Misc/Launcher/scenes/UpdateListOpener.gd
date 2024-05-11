34a614325fc55d66bbc2d93a709b25a16f3b61fe
mahikotahi <portilizenseason@gmail.com>
2024-05-11 16:07:43 -0600

    leather128 helped with the reading file code!

--- Misc/Launcher/scenes/UpdateListOpener.gd
+++ Misc/Launcher/scenes/UpdateListOpener.gd
@@ -5,15 +5,20 @@ extends TextureButton
 @onready var description = $"../UpdateThing/Description"
 
 func _ready():
-	link_button.anchors_preset = PRESET_CENTER
-	# link_button.uri = load("assets/updates/current_update.txt")
+	# link_button.anchors_preset = PRESET_CENTER
+	link_button.uri = load_file("assets/updates/update-link.txt")
+	# link_button.text = load_file("assets/updates/update-link.txt")
+	description.text = "\n"+load_file("assets/updates/update-description.txt")
 	
-	# description.text = load("assets/updates/current_update.txt")
-	
-	# print(load("updates/current_update.txt"))
-	description.text = "\ntest"
+	# print(load_file("assets/updates/current_update.txt"))
+	# description.text = "\ntest"
 
 func _on_pressed():
 	# load("assets/updates/current_update.txt")
 	update_thing.visible = true
 	# link_button.uri = load("assets/updates/current_update.txt")
+
+func load_file(fileN):
+	var file = FileAccess.open("res://"+fileN, FileAccess.READ)
+	var content = file.get_as_text()
+	return content
