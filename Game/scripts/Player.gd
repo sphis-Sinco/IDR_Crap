7958b59fc00dc6e5b016ff03d71458cfe62303aa
mahikotahi <portilizenseason@gmail.com>
2024-05-12 08:55:25 -0600

    decrease wait time

--- Game/scripts/Player.gd
+++ Game/scripts/Player.gd
@@ -4,10 +4,17 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
+var multiplier = 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	if Global.DOUBLE_SPEED:
+		multiplier = 2
+		print_rich('[color=cyan]DOUBLE SPEED MODE![/color]')
+	if Global.SPEEDRUN_MODE:
+		print_rich('[color=cyan]SPEEDRUN MODE![/color]')
 
 func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -24,7 +31,7 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY * multiplier
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,8 +42,8 @@ func _physics_process(delta):
 		animated_sprite_2d.flip_h = true
 	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * (SPEED * multiplier)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, (SPEED * multiplier))
 
 	move_and_slide()
